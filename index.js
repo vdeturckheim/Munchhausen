300b5db8875cdb6dd567e0bb7faabcaee9e269fc
vladimir <vlad2t@hotmail.com>
2015-12-07 21:30:58 +0100

    dyupid stuff i wanna save

--- index.js
+++ index.js
@@ -8,25 +8,66 @@ server.connection({ port: 8000, labels: 'puppet' });
 
 let incr = 0;
 
-server.select('master').route({
-    method: 'GET',
-    path: '/1',
-    handler: function (request, reply) {
+const routeStore = {};
+const Route = require('./lib/route');
 
-        request.server.select('puppet').route({
-            method: 'GET',
-            path: '/' + incr,
-            handler: function (_request, _reply){
+server.select('master')
+    .route([
+        {
+            method: 'POST',
+            path: '/routes',
+            handler: function (request, reply) {
 
-                return _reply('puppet');
+                const route = new Route(request.payload.method, request.payload.path, request.payload.responseCode,
+                    request.payload.responseContent);
+                const uniqueId = request.payload.method + '_' + request.payload.path;
+
+                if (routeStore[uniqueId]){
+                    return reply(Boom.conflict());
+                }
+
+                routeStore[uniqueId] = route;
+                console.log(routeStore);
+
+                server.select('puppet').route(route.getRoute());
+                reply(route.getId()).code(201);
+
             }
-        });
-        incr++;
+        },
+        {
+            method: 'GET',
+            path: '/routes',
+            handler: function (request, reply){
 
-        return reply(Boom.notFound());
-    }
-});
+                const keys = Object.keys(routeStore);
+                const resp = [];
+                keys.forEach((key) => {
 
+                    resp.push(routeStore[key].print());
+                });
+
+                reply(resp);
+            }
+        },
+        {
+            method: 'PUT',
+            path: '/routes/{id}',
+            handler: function (request, reply){
+                const uniqueId = request.payload.method + '_' + request.payload.path;
+                const route = routeStore[uniqueId];
+                console.log(routeStore);
+                console.log(route);
+                if (request.payload.responseCode){
+                    route.setResponseCode(request.payload.responseCode);
+                }
+                if (request.payload.responseContent){
+                    route.setResponseContent(request.payload.responseContent);
+                }
+
+                reply(route.print());
+            }
+        }
+    ]);
 
 
 server.start(() => {
